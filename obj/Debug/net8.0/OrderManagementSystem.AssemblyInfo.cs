53632e6f64fc8dabbac1b1e7d7c0592ef5650839
Pryce <85907457+PryceWayne@users.noreply.github.com>
2024-09-21 00:57:39 -0400

    Shipping_API_Branch
    Creation of Shipping API branch

--- obj/Debug/net8.0/OrderManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/OrderManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e647deb483f62c70929d4a9e16d7361b59a771c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd3f5450d26e8855717e529380956745f66da53")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
